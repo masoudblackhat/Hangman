b5ff30f3e7bd0904a8e9311770ad562807f0ca8d
hamid_hajilo <hamid.t711@gmail.com>
2018-02-14 04:29:33 +0330

    setText for test

--- app/src/main/java/com/example/masoud/hagman/MainActivity.java
+++ app/src/main/java/com/example/masoud/hagman/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.masoud.hagman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView txtWordView;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+      txtWordView =(TextView)findViewById(R.id.txtWordView);
 
     int a=0;
     int c=2;
   }
   public void onClick(View view){
     String letter=view.getTag().toString();
+
+      txtWordView.setText(letter);
 
   }
 }
