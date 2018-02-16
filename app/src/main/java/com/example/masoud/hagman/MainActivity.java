c6304cbf8870e80925603eae57f40160a98294ce
Mr.Robot <yusufzk78@gmail.com>
2018-02-16 13:38:30 +0330

    This is test for First Coding in Github

--- app/src/main/java/com/example/masoud/hagman/MainActivity.java
+++ app/src/main/java/com/example/masoud/hagman/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 TextView txtWordView;
@@ -15,7 +16,7 @@ TextView txtWordView;
 
     int a=0;
     int c=2;
-
+      Toast.makeText(this, "Erro ", Toast.LENGTH_SHORT).show();
     int g=0;
 
 
@@ -25,7 +26,7 @@ TextView txtWordView;
   }
   public void onClick(View view){
      String letter = view.getTag().toString();
-
+        
       txtWordView.setText(letter + "Yusuf Zinsaz");
 
 
